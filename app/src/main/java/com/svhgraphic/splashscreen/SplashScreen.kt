587122213036a6d13b90fd82b5d50a5d7d2238f5
Sarvesh-Panhekar <sarvesh.panhekar@gmail.com>
2021-08-24 15:52:23 +0530

    Splash screen build successfully.

--- app/src/main/java/com/svhgraphic/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/svhgraphic/splashscreen/SplashScreen.kt
@@ -1,4 +1,23 @@
 package com.svhgraphic.splashscreen
 
-class SplashScreen {
-}
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+private const val SPLASH_TIME: Long = 3000
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+      Handler().postDelayed({
+startActivity(Intent (this,MainActivity::class.java))
+          finish()
+
+
+               }, SPLASH_TIME)
+        }
+       }
